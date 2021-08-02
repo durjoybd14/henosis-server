f8410c2ea290452f341d2224ef2f1b9ded633bc4
saifulislamsojib <saifulislam.sojib56@gmail.com>
2021-08-02 23:50:07 +0600

    workspace-finish

--- index.js
+++ index.js
@@ -1,45 +1,57 @@
-//require packages
-const express = require("express");
-const mongoose = require("mongoose");
-require("dotenv").config();
+// require packages
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const { server, io } = require('./socket');
+const { createWorkspace, singleWorkspace, userWorkspaces } = require('./socketHandler/workspace');
+require('dotenv').config();
 
-//server port
+// server port
 const port = process.env.PORT || 5000;
 
-//express app initialization
+// express app initialization
 const app = express();
 app.use(express.json());
 
-const cors = require("cors");
 app.use(cors());
 
-//database connection with mongoose
+// database connection with mongoose
 mongoose
-  .connect(
-    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ghclx.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
-  .then(() => console.log("Database connected successfully"))
-  .catch((error) => console.log("ERROR", error));
+    .connect(
+        `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ghclx.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        },
+    )
+    .then(() => console.log('Database connected successfully'))
+    .catch((error) => console.log('ERROR', error));
 
-//application routes
+// application routes
 
-//root route
-app.get("/", (req, res) => {
-  res.send("Henosis server is running");
+// root route
+app.get('/', (req, res) => {
+    res.send('Henosis server is running');
 });
 
+const createWorkspaceN = io.of('/create-workspace');
+createWorkspaceN.on('connection', createWorkspace);
+
+const singleWorkspaceN = io.of('/workspace');
+singleWorkspaceN.on('connection', singleWorkspace);
+
+const userWorkspacesN = io.of('/user-workspaces');
+userWorkspacesN.on('connection', userWorkspaces);
+
 // default error handler
+// eslint-disable-next-line consistent-return
 function errorHandler(err, req, res, next) {
-  if (res.headerSent) {
-    return next(err);
-  }
-  res.status(500).json({ error: err });
+    if (res.headerSent) {
+        return next(err);
+    }
+    res.status(500).json({ error: err });
 }
 
-app.listen(port, () => {
-  console.log(`Boss! I am listening to you at port:${port}`);
+server.listen(port, () => {
+    console.log(`Boss! I am listening to you at port:${port}`);
 });
