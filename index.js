6f95b34a8c7b8169a80350e7a875d97fb05cd8fb
nrd141913 <nrd141913@.com>
2021-08-02 16:18:46 +0600

    stripe added

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 //require packages
 const express = require("express");
 const mongoose = require("mongoose");
+const stripe = require("stripe")(`${process.env.STRIPE_SECRET_KEY}`);
+const uuid = require("uuid");
 require("dotenv").config();
 
 //server port
@@ -27,18 +29,55 @@ mongoose
 
 //application routes
 
+app.post("/payment", (req, res) => {
+  const { henoPackage, token } = req.body;
+  console.log("henoPackage", henoPackage);
+  console.log("price", henoPackage.price);
+  const idempontencyKey = uuid();
+
+  return stripe.customers
+    .create({
+      email: token.email,
+      source: token.id,
+    })
+    .then((customer) => {
+      stripe.charges.create(
+        {
+          amount: product.price * 100,
+          currency: "usd",
+          customer: customer.id,
+          receipt_email: token.email,
+          description: `purchase package ${product.name}`,
+          shipping: {
+            name: token.card.name,
+            address: {
+              country: token.card.address_country,
+            },
+          },
+        },
+        { idempontencyKey }
+      );
+    })
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((err) => console.log(err));
+});
+
 //root route
 app.get("/", (req, res) => {
   res.send("Henosis server is running");
 });
 
 // default error handler
-function errorHandler(err, req, res, next) {
+const errorHandler = (err, req, res, next) => {
   if (res.headerSent) {
     return next(err);
   }
   res.status(500).json({ error: err });
-}
+};
+
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Boss! I am listening to you at port:${port}`);
