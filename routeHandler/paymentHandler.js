d49c192355dffe5af8c76bfd54b63d9b191efbc1
saifulislamsojib <saifulislam.sojib56@gmail.com>
2021-08-05 16:28:17 +0600

    workspace-schema

--- routeHandler/paymentHandler.js
+++ routeHandler/paymentHandler.js
@@ -1,29 +1,27 @@
-/* eslint-disable arrow-body-style */
-/* eslint-disable quotes */
 /* eslint-disable prettier/prettier */
-const express = require("express");
+const express = require('express');
 
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const router = express.Router();
-require("dotenv").config();
+require('dotenv').config();
 
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const paymentSchema = new mongoose.Schema({}, { strict: false });
-const Payment = new mongoose.model("Payment", paymentSchema);
+const Payment = new mongoose.model('Payment', paymentSchema);
 
-router.post("/", (req, res) => {
+router.post('/', (req, res) => {
   const token = req.body;
   const newPayment = new Payment(req.body);
   newPayment.save((err) => {
     if (err) {
       res.status(500).json({
-        error: "There was a server side error!",
+        error: 'There was a server side error!',
       });
     } else {
       res.status(200).json({
-        message: "Payment story was recorded successfully!",
+        message: 'Payment story was recorded successfully!',
       });
     }
   });
@@ -38,12 +36,12 @@ router.post("/", (req, res) => {
         .create({
           amount: 60 * 100,
           customer: customer.id,
-          currency: "usd",
+          currency: 'usd',
           email: token.email,
         })
         .then((invoiceItem) => {
           return stripe.invoices.create({
-            collection_method: "send_invoice",
+            collection_method: 'send_invoice',
             customer: invoiceItem.customer,
             days_until_due: 10,
             email: token.email,
