ab36fc7b80bc51bc6f9e946c8d01f22941be3c31
nrd141913 <nrd141913@.com>
2021-08-20 00:14:55 +0600

    img url resolved

--- src/routeHandler/userHandler.ts
+++ src/routeHandler/userHandler.ts
@@ -56,7 +56,7 @@ router.get('/:email', async (req, res) => {
 });
 
 router.put('/:email', (req, res) => {
-    const result = User.findOneAndUpdate(
+    const result = User.findByEmailAndUpdate(
         { email: req.params.email },
         {
             $set: {
