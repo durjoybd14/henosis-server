0de95378c87cd42df758982b9c27d8ecd0418474
nrd141913 <nrd141913@.com>
2021-09-01 14:26:18 +0600

    sprint by workspace added

--- src/routeHandler/sprintWorkspaceHandler.ts
+++ src/routeHandler/sprintWorkspaceHandler.ts
@@ -3,11 +3,12 @@ import mongoose from 'mongoose';
 import sprintSchema from '../schemas/sprintSchema';
 
 const router = express.Router();
-const SprintModel = mongoose.model('SprintModal', sprintSchema);
+
+const Sprint = mongoose.model('sprint', sprintSchema);
 
 router.get('/:id', async (req, res) => {
     try {
-        const data = await SprintModel.find({ workspaceId: req.params.id });
+        const data = await Sprint.find({ workspaceId: req.params.id });
         res.status(200).json({
             data,
             message: 'Sprint info by workspaceId was collected successfully!',
